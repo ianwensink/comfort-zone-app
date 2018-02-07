3890246900faaaf3794fcaa3d1b6bdd0b1161db8
Ian Wensink <ian@burst-digital.com>
2018-02-07 21:53:18 +0100

    fix(timeline): extend timeline item

--- src/components/timeline/TimelineItem.js
+++ src/components/timeline/TimelineItem.js
@@ -2,13 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { View, Text } from 'react-native';
 import { ListItem } from 'react-native-elements';
+import moment from 'moment';
 
 // Consts and Libs
 import { AppStyles } from '../../theme';
 
+const formatSubtitle = event => moment(event.timestamp).fromNow();
+
 /* Component ==================================================================== */
 const TimelineItem = ({ event, navigation }) => (
-  <ListItem onPress={() => navigation.navigate('EventDetail', event)} title={event.label} />
+  <ListItem
+    onPress={() => navigation.navigate('EventDetail', event)}
+    title={event.label}
+    subtitle={formatSubtitle(event)}
+  />
 );
 
 TimelineItem.propTypes = { event: PropTypes.shape({}) };
